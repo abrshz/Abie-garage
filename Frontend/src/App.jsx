69db944342a9651fe566ca844b21bec956862966
abrshz <trusewynur@gmail.com>
2025-11-09 23:19:16 +0300

    Created AddEmploye component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,8 @@
 import './App.css'
+// Import the Routes and Route from react-dom
+import {Routes, Route} from 'react-dom';
+// Import page components 
+
 
 function App() {
 
---
+++ Frontend/src/markup/pages/admin/AddEmploye.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddEmploye() {
+  return (
+    <></>
+)
+}
+
+export default AddEmploye
